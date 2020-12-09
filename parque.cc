a224805fc74f3d17f1feda791ec396bdafbfd72d
rfaromero12 <rafael24.07.2001@gmail.com>
2020-12-09 17:42:42 +0100

    ficheros_parque

--- parque.cc
+++ parque.cc
@@ -1,1 +1,36 @@
-__cpp_user_defined_literalsdsdsd
+#include "parque.h"
+using namespace std;
+
+Parque::Parque(string nombreparque_,float superficie_,float ubicacion_,string localizacion_)
+{
+    nombreparque_ = nombreparque;
+    superficie_ = superficie;
+    ubicacion_ = ubicacion;
+    localizacion_ = localizacion;
+}
+void Parque::addPremios(string premios){
+    
+}
+void Parque::addSendero(Sendero senderos){
+    for(list<Sendero>::iterator it = senderos_.begin(); it != senderos_.end(); it++){
+        if(it->getCodigo() == senderos.getCodigo()){
+            cout << "El sendero ya esta registrado" << endl;
+            exit(1);
+        }
+    }
+    senderos_.push_back(senderos);
+    cout << "El sendero ha sido registrado" << endl;
+}
+void Parque::Cancelar_ruta(Ruta rutas){
+
+    for(vector<Ruta>::iterator it = rutas_.begin(); it != rutas_.end();it++){
+        if(it->getNumeroruta() == rutas.getNumeroruta()){
+            rutas_.erase(it);
+            cout << "La ruta ha sido cancelada con exito" << endl;
+        }
+    }
+    cout << "No se ha encontrado la ruta que se queria borrar" << endl;
+}
+
+//seleccionar_ruta
+//seleccionar_sendero
---
+++ parque.h
@@ -0,0 +1,43 @@
+#ifndef _PARQUE.h_
+#define _PARQUE.h_
+
+#include <iostream>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+
+class Parque
+{
+private:
+    /* data */
+    string nombreparque_;
+    float superficie_;
+    float ubicacion_;
+    string localizacion_;
+    list<string>premios_;
+    vector<Sendero>senderos_;
+    vector<Ruta>rutas_;
+
+public:
+    Parque(string nombreparque,float superficie = 0.0,float ubicacion = 0.0,string localizacion = "");
+    inline string getNombre()const{return nombreparque_;}
+    inline float getSuperficie()const{return superficie_;}
+    inline float getUbicacion()const{return ubicacion_;}
+    inline list<string> getPremios()const{return premios_;}
+    inline vector<Sendero> getSenderos()const{return senderos_;}
+    inline vector<Ruta> getRutas()const{return rutas_;}
+    inline void setSuperficie(float superficie){superficie_ = superficie;}
+    inline void setUbicacion(float ubicacion){ubicacion_ = ubicacion;}
+    inline void setLocalizacion(string localizacion){localizacion_ = localizacion;}
+    void addPremios(string premios);
+    void addSendero(Sendero senderos);//deberia ser bool por si mete un sendero que ya esta dentro
+    Ruta Seleccionar_ruta();//hablar de esta funcion
+    Sendero Seleccionar_sendero();//hablar de esta funcion
+    void Cancelar_ruta(Ruta rutas);
+};
+
+
+
+#endif
