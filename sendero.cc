8acf20fa312ba47c3eaa73e6a10ce3b8b8877ef6
rfaromero12 <rafael24.07.2001@gmail.com>
2020-12-10 12:42:32 +0100

    actualizacion sendero

--- sendero.cc
+++ sendero.cc
@@ -2,11 +2,18 @@
 
 using namespace std;
 //para poder hacer el commit
-Sendero::Sendero(string codigo,Parque parque,string dificultad,string disponibilidad,string longitud)
+Sendero::Sendero(string codigo,Parque parque,string dificultad,string disponibilidad,float longitud)
 {
     setCodigo(codigo);
     setParque(parque);
     dificultad_ = dificultad;
     disponibilidad_ = disponibilidad;
     longitud_ = longitud;
+}
+bool Sendero::setLongitud(float longitud){
+    if(longitud >= 0){
+        longitud_ = longitud;
+        return true;
+    }
+    return false;
 }
--- sendero.h
+++ sendero.h
@@ -11,18 +11,18 @@ private:
     string codigo_;
     string dificultad_;
     string disponibilidad_;
-    string longitud_;
+    float longitud_;
     Parque parque_;
 public:
-    Sendero(string codigo,Parque parque,string dificultad = "", string disponibilidad = "",string longitud);
+    Sendero(string codigo,Parque parque,string dificultad = "", string disponibilidad = "",float longitud = "");
     inline string getCodigo()const{return codigo_;}
     inline string getDificultad()const{return dificultad_;}
-    inline string getLongitud()const{return longitud_;}
+    inline float getLongitud()const{return longitud_;}
     inline string getDisponibilidad()const{return disponibilidad_;}
     inline Parque getParque()const{return parque_;}
     inline void setCodigo(string codigo){codigo_ = codigo;}
     inline void setDificultad(string dificultad){dificultad_ = dificultad;}
-    inline void setLongitud(string longitud){longitud_ = longitud;}
+    inline bool setLongitud(string longitud)
     inline void setDisponibilidad(string disponibilidad){disponibilidad_ = disponibilidad;}
     inline void setParque(Parque parque){parque_ = parque;}
 };
