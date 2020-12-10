f67fd486edd37ea7016bf1bc54dd73f063431187
Victorrojass <victorrrojasmunoz@gmail.com>
2020-12-10 12:02:39 +0100

    cambiosmnitor

--- Monitor.cc
+++ Monitor.cc
@@ -9,42 +9,44 @@
 #include "estructuras.h"
 using namespace std;
 
-Fecha::Fecha(){
+Fecha:: Fecha(){
     dia=0;
     mes=0;
     año=0;
 }
 
-Monitor:: Monitor(){
-	Nombre_='0';
-	Apellidos_= '0';
-	Fecha_= 0;
-	DNI_= '0';
-	Correo_= '0';
-	Telefono_=0;
+Monitor:: Monitor(string Monitor, string Apellidos, Fecha Fecha, string DNI, string Correo, int Telefono){
+	Nombre_=Nombre;
+	Apellidos_= Apellidos;
+	Fecha_= Fecha;
+	DNI_= DNI;
+	Correo_= Correo;
+	Telefono_= Telefono;
 }
 
 //Inicializo los valores de la estructuera monitor
 
 
-string Monitor::setNombre(){
+string Monitor::setNombre(Nombre){
 	cout<<"Nombre: ";
 	cin>>Nombre_;
+    Nombre_=Nombre;
 } //Funcion para introducir el nombre de un nuevo Monitor
 
 
-void Monitor::getNombre(){
+void Monitor::getNombre(Nombre){
 	cout<<"Nombre: "<<Nombre_<<"\n";
 } //Funcion para obtener el nombre del Monitor seleccionado
 
 
-string Monitor::setApellidos(){
+string Monitor::setApellidos(Apellidos){
 	cout<<"Apellidos: ";
 	cin>>Apellidos_;
+    Apellidos_=Apellidos;
 } //Funcion para introducir apellidos de un nuevo Monitor 
 
 
-void Monitor::getApellidos(){
+void Monitor::getApellidos(Apellidos){
 	cout<<"Apellidos: "<<Apellidos_<<"\n";
 } //Funcion para obtener los apellidos del Monitor seleccionado
 
@@ -66,34 +68,37 @@ void Monitor::getFecha(){
 } //Funcion para obtener la fecha de nacimiento del Monitor seleccionado
 
 
-string Monitor::setDNI(){
+string Monitor::setDNI(DNI){
 	cout<<"DNI: ";
 	cin>>DNI_;
+    DNI_=DNI;
 } //Funcion para introducir el DNI de un nuevo Monitor
 
 
-void Monitor::getDNI(){
+void Monitor::getDNI(DNI){
 	cout<<"DNI: "<<DNI_<<"\n";
 } //Funcion para obtener el DNI del Monitor seleccionado
 
 
-string Monitor::setCorreo(){
+string Monitor::setCorreo(Correo){
 	cout<<"Correo electronico: ";
 	cin>>Correo_;
+    Correo_=Correo;
 } //Funcion para introducir el correo electronico de un nuevo Monitor
 
 
-void Monitor::getCorreo(){
+void Monitor::getCorreo(Correo){
 	cout<<"Correo electronico: "<<Correo_<<"\n";
 } //Funcion para obtener el correo electronico del Monitor seleccionado
 
 
-int Monitor::setTelefono(){
+int Monitor::setTelefono(Telefono){
 	cout<<"Telefono: ";
 	cin>>Telefono_;
+    Telefono_=Telefono;
 } //Funcion para introducir el numero de telefono de un nuevo Monitor
 
 
-void Monitor::getTelefono(){
+void Monitor::getTelefono(Telefono){
 	cout<<"Telefono: "<<Telefono_<<"\n";
 } //Funcion para obtener el numero de Telefono del monitor seleccionado
