9edf05276f03029f3a8e9c01cde9a10311ca4285
Victorrojass <victorrrojasmunoz@gmail.com>
2020-12-10 11:41:24 +0100

    comentarios monitores

--- Monitor.cc
+++ Monitor.cc
@@ -24,7 +24,7 @@ Monitor:: Monitor(){
 string Monitor::setNombre(){
 	cout<<"Nombre: ";
 	cin>>Nombre_;
-} //Funcion para introducir el nombre
+} //Funcion para introducir el nombre de un nuevo Monitor
 
 
 void Monitor::getNombre(){
@@ -35,45 +35,53 @@ void Monitor::getNombre(){
 string Monitor::setApellidos(){
 	cout<<"Apellidos: ";
 	cin>>Apellidos_;
-} //Funcion para introducir apellidos del monitor 
+} //Funcion para introducir apellidos de un nuevo Monitor 
 
 
 void Monitor::getApellidos(){
 	cout<<"Apellidos: "<<Apellidos_<<"\n";
 } //Funcion para obtener los apellidos del Monitor seleccionado
 
+
 float Monitor::setFecha(){
 	cout<<"Fecha: ";
 	cin>>Fecha_;
-}
+} //Funcion para introducir la fecha de nacimiento de un nuevo Monitor
+
 
 void Monitor::getFecha(){
 	cout<<"Fecha :"<<Fecha_<<"\n";
-}
+} //Funcion para obtener la fecha de nacimiento del Monitor seleccionado
+
 
 string Monitor::setDNI(){
 	cout<<"DNI: ";
 	cin>>DNI_;
-}
+} //Funcion para introducir el DNI de un nuevo Monitor
+
 
 void Monitor::getDNI(){
 	cout<<"DNI: "<<DNI_<<"\n";
-}
+} //Funcion para obtener el DNI del Monitor seleccionado
+
 
 string Monitor::setCorreo(){
 	cout<<"Correo electronico: ";
 	cin>>Correo_;
-}
+} //Funcion para introducir el correo electronico de un nuevo Monitor
+
 
 void Monitor::getCorreo(){
 	cout<<"Correo electronico: "<<Correo_<<"\n";
-}
+} //Funcion para obtener el correo electronico del Monitor seleccionado
+
 
 int Monitor::setTelefono(){
 	cout<<"Telefono: ";
 	cin>>Telefono_;
-}
+} //Funcion para introducir el numero de telefono de un nuevo Monitor
+
 
 void Monitor::getTelefono(){
 	cout<<"Telefono: "<<Telefono_<<"\n";
-}
+} //Funcion para obtener el numero de Telefono del monitor seleccionado
