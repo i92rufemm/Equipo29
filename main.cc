bb07defc2dfca8ecfec48efd9d8df3281ce30582
rfaromero12 <rafael24.07.2001@gmail.com>
2020-12-20 16:57:16 +0100

    main parque

--- main.cc
+++ main.cc
@@ -15,20 +15,35 @@ int main(){
     
     int op;
     string key;
-    
+    if(parque.lee_senderos_parque() == false){
+        cout << "Error al leer los datos del fichero sendero" << endl;
+        exit(-1);
+    }
+    if(parque.lee_rutas() == false){
+        cout << "Error al leer los datos del fichero rutas" << endl;
+        exit(-1);
+    }
+    if(parque.lee_monitores_parque() == false){
+        cout << "Error al leer los datos de los monitores" << endl;
+        exit(-1);
+    }
+    if(parque.lee_clientes_parque() == false){
+        cout << "Error al leer los datos de los clientes del parque" << endl;
+        exit(-1);
+    }
     cout << "Bienvenid@ a la aplicacion de los parques naturales de Cordoba\n ¿que quieres hacer?" << endl;
     
     do{
         do{
             cout << "(1)Registrar nuevo sendero" << endl;                
-            cout << "(2)Registrar nuevo sendero" << endl;
-            cout << "(3)Registrar nuevo sendero" << endl;
-            cout << "(4)Registrar nuevo sendero" << endl;
-            cout << "(5)Registrar nuevo sendero" << endl;
-            cout << "(6)Registrar nuevo sendero" << endl;
-            cout << "(7)Registrar nuevo sendero" << endl;
-            cout << "(8)Registrar nuevo sendero" << endl;
-            cout << "(9)Registrar nuevo sendero" << endl;
+            cout << "(2)Registrar nuevo cliente" << endl;
+            cout << "(3)Registrar nuevo monitor" << endl;
+            cout << "(4)Registrar nuevo parque" << endl;
+            cout << "(5)" << endl;
+            cout << "(6)" << endl;
+            cout << "(7)" << endl;
+            cout << "(8)" << endl;
+            cout << "(9)" << endl;
 
             cin >> op;
 
@@ -36,6 +51,13 @@ int main(){
 
         switch(op){
             case 1:
+                string dificultad,disponibilidad;
+                cout << "Dificultad(): ";
+                getline(cin,dificultad);
+                sendero.setDificultad(dificultad);
+                cout << "Disponibilidad(): ";
+                getline(cin,disponibilidad);
+                cout << endl
 
             break;
 
@@ -43,7 +65,7 @@ int main(){
 
     }while(key == "si" || key == "Si");
 
-
+    
 
 
 
